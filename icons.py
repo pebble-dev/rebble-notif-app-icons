c2c90af5265401d31e60720d8f5850d2fa316e49
Katharine Berry <ktbry@google.com>
2021-06-04 01:01:02 -0700

    add some special iOS icons

--- icons.py
+++ icons.py
@@ -17,6 +17,57 @@ from flask import Flask, request, abort, send_file
 from PIL import Image
 import requests
 
+SPECIAL_APPLE_APPS = set((
+  "com.Apple.Magnifier",
+  "com.apple.AppStore",
+  "com.apple.Bridge",
+  "com.apple.DocumentsApp",
+  "com.apple.Fitness",
+  "com.apple.Health",
+  "com.apple.Home",
+  "com.apple.Keynote",
+  "com.apple.Maps",
+  "com.apple.MobileAddressBook",
+  "com.apple.MobileSMS",
+  "com.apple.MobileStore",
+  "com.apple.Music",
+  "com.apple.Numbers",
+  "com.apple.Pages",
+  "com.apple.Passbook",
+  "com.apple.Photo-Booth",
+  "com.apple.Playgrounds",
+  "com.apple.Preferences",
+  "com.apple.Translate",
+  "com.apple.VoiceMemos",
+  "com.apple.calculator",
+  "com.apple.camera",
+  "com.apple.clips",
+  "com.apple.compass",
+  "com.apple.facetime",
+  "com.apple.findmy",
+  "com.apple.iBooks",
+  "com.apple.iCloudDriveApp",
+  "com.apple.iMovie",
+  "com.apple.measure",
+  "com.apple.mobilecal",
+  "com.apple.mobilegarageband",
+  "com.apple.mobilemail",
+  "com.apple.mobilenotes",
+  "com.apple.mobilephone",
+  "com.apple.mobilesafari",
+  "com.apple.mobileslideshow",
+  "com.apple.mobiletimer",
+  "com.apple.news",
+  "com.apple.podcasts",
+  "com.apple.reminders",
+  "com.apple.shortcuts",
+  "com.apple.stocks",
+  "com.apple.store.Jolly",
+  "com.apple.tips",
+  "com.apple.tv",
+  "com.apple.weather"
+))
+
 app = Flask(__name__)
 
 if environ.get('HONEYCOMB_KEY'):
@@ -47,6 +98,10 @@ def rescale_image(original_url, size, out_f):
 
 @app.route('/ios/<string:bundle_id>/<int:size>.jpg')
 def image(bundle_id, size):
+    if bundle_id in SPECIAL_APPLE_APPS:
+        response = requests.get(f"https://storage.googleapis.com/ios-standard-icons/{bundle_id}.jpeg", stream=True)
+        response.raise_for_status()
+        return send_file(response.raw, mimetype='image/jpeg', cache_timeout=2592000)
     country = request.args.get('country')
     url = get_apple_image_url(bundle_id, country)
     f = io.BytesIO()
